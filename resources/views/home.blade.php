9277764a090623b2e1a06c8a910a23948c7d04af
Luciano <lucco6823@gmail.com>
2021-10-29 10:06:13 +0200

    aggiunta css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card">
+<div class="single-movie">
     @foreach ($movies as $movie)
-        <p>{{ $movie['title'] }}</p>
+        <div class="movie">
+            <p>TITOLO: {{ $movie['title'] }}</p>
+            <p>TITOLO ORIGINALE: {{ $movie['original_title'] }}</p>
+            <p>NAZIONALIÁ: {{ $movie['nationality'] }}</p>
+            <p>DATA DI USCITA: {{ $movie['date'] }}</p>
+            <p>VOTO: {{ $movie['vote'] }}</p>
+        </div>     
     @endforeach
 </div>
 @endsection
